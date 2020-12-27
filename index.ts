bf44e3f7feb3ab55d1d954b42b4901e026ecd9d7
friendmy <friendmytrue@gmail.com>
2020-12-28 08:51:23 +0900

    0.4 First Steps

---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = "Nicolas",
+    age = 24,
+    gender = "mail";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
+}
+
+sayHi(name, age)
+
+export {};
